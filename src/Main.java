51705f557154293aaeb201b2fa537f289aa5f2e0
Inna <serebryakovainna@mail.ru>
2022-10-29 07:12:05 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 50;
+        if (age >= 18) {
+            System.out.println("Поздраавляем с совершеннолетием!");
+        }
+        if (age < 18) {
+            System.out.println("Тебе надо немного подождать, пока тебе не исполнится 18. Тогда наступит твое совершеннолетие!");
+        }
+        }
+    }

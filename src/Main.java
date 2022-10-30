f38594eb7bfff28b5e5791236ca6a3fca4d1ab1a
Inna <serebryakovainna@mail.ru>
2022-10-30 08:06:38 +0200

    Внесли изменения 9

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,11 @@ public class Main {
             System.out.println("Тебе надо немного подождать, пока тебе не исполнится 18. Тогда наступит твое совершеннолетие!");
         }
         System.out.println("Exercise2");
-        int age1 = 30;
+        int age1 = 10;
         if (age1 >= 7 && age1 < 18) {
             System.out.println("Ребенок ходит в школу.");
         }
-        if (age1 >= 18 && age1 < 24) {
+        else if (age1 >= 18 && age1 < 24) {
             System.out.println("Человек уже закончил школу и может отправляться в университет.");
         } else {
             System.out.println("Человек окончил университет и ему пора искать первую работу.");
@@ -24,30 +24,32 @@ public class Main {
         if (saleSitPlace < 60 && saleStayPlace < trafficWagon - sitTrWagon) {
             System.out.println("В вагоне есть " + (sitTrWagon - saleSitPlace) + " сидячих мест, " + ((trafficWagon - sitTrWagon) - saleStayPlace) + " стоячих мест.");
         }
-        if (saleSitPlace >= 60 && saleStayPlace < trafficWagon - sitTrWagon) {
+        else if (saleSitPlace >= 60 && saleStayPlace < trafficWagon - sitTrWagon) {
             System.out.println("В вагоне есть " + (sitTrWagon - saleSitPlace) + " сидячих мест, " + ((trafficWagon - sitTrWagon) - saleStayPlace) + " стоячих мест.");
         }
-        if (saleSitPlace < 60 && saleStayPlace >= trafficWagon - sitTrWagon) {
+        else if (saleSitPlace < 60 && saleStayPlace >= trafficWagon - sitTrWagon) {
             System.out.println("В вагоне есть " + (sitTrWagon - saleSitPlace) + " сидячих мест, " + ((trafficWagon - sitTrWagon) - saleStayPlace) + " стоячих мест.");
         } else {
             System.out.println("В вагоне нет сободных мест.");
         }
-        int age2 = 1;
+        // Возраст учиться
+        int age2 = 19;
         if (age2 >= 2 && age2 <= 6) {
             System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в детский сад.");
         }
-        if (age2 >= 7 && age2 <= 18) {
+        else if (age2 >= 7 && age2 <= 18) {
             System.out.println("Если возраст человека равен " + age2 + ", то ему нужно ходить в школу.");
         }
-        if (age2 > 18 && age2 <= 24) {
+        else if (age2 > 18 && age2 <= 24) {
             System.out.println("Если возраст человека равен " + age2 + ", то его мместо в университете.");
         }
-        if (age2 > 24 && age2 < 65) {
+        else if (age2 > 24 && age2 < 65) {
             System.out.println("Если возраст человека равен " + age2 + ", то ему пора ходить на работу.");
         }
         else {
             System.out.println("Если человеку меньше 2 лет и больше 65, ему не нужно ходить на работу или в школу.");
         }
+        // Аттракционы
         int ageAttr = 30;
         if (ageAttr > 0 && ageAttr < 5) {
             System.out.println("Кататься на аттракционах запрещено!");
@@ -57,6 +59,20 @@ public class Main {
         }
         else {
             System.out.println("Кататься на аттракционах можно самостоятельно.");
+        }
+        // условный оператор 8.Задание 3
+        int one = 1;
+        int two = 3;
+        int tree = 2;
+        if ((two > one && tree > two) || (one > two && tree > one)) {
+            System.out.println("Большее число tree.");
+        }
+        // Какое число больше
+        else if ((tree > one && two > tree) || (one > tree && two > one)) {
+            System.out.println("Большее число two.");
+        }
+        else {
+            System.out.println("Большее число one.");
         }
         }
     }
